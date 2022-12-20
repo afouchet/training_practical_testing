0acdbc81f25e910558d504c9c61e8650b177acf5
Arnaud Fouchet <arnaud@talk2me.io>
2022-12-20 18:06:08 +0100

    Added placeholder

--- src/distribution.py
+++ src/distribution.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 
-def compute_distribution_1_week(df_sales):
+def compute_distribution_1_week(df_sales: pd.DataFrame) -> pd.DataFrame:
     return pd.read_csv("tests/assets/result_compute_distribution.csv")
